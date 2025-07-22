a8f3a8f038c525afd11d723c564c43a0f746507d
Alex Wu <alexwu1705@users.noreply.github.com>
2025-07-22 11:42:18 +0700

    fix not regenerate saltNonce

--- packages/api-service/src/dungeon/dungeon.service.ts
+++ packages/api-service/src/dungeon/dungeon.service.ts
@@ -118,6 +118,13 @@ export class DungeonService {
         },
       },
     );
+
+    await this.dropGemModel.create({
+      player: player._id,
+      gameId: newProgress.gameId.toString(),
+      gem: 0,
+      saltNonce: Math.floor(Date.now() / 1000),
+    });
     return result;
   }
 
